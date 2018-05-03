6d8e9d660a935eabf75e7dba2a25ba77c09470c6
D7703_11 <d7703_11@d7703-11ui-imac.local>
2018-05-03 15:42:39 +0900

    simpleTableView

--- simpletableView/ViewController.swift
+++ simpletableView/ViewController.swift
@@ -8,11 +8,17 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController,UITableViewDataSource,UITableViewDelegate {
+    
+    var animals =  ["Cat","Tiger","Lion","Monkey","Cow"]
+    var country = ["Korea","China","Africa","Japan","Aus"]
+    @IBOutlet weak var myTableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        //delegate 와 viewController의 연결
+        myTableView.dataSource = self
+        myTableView.delegate = self
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +26,28 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    // row의 수
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return animals.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        // cell의 재사용
+        let identifier = "RE"
+        // cell을 하나 메모리 할당하고 재사용을 위해 저장("RE")
+        let cell = myTableView.dequeueReusableCell(withIdentifier: identifier, for: indexPath)
+        
+        // 배열에 있는 데이터를 cell에 넣기
+        cell.textLabel?.text = animals[indexPath.row]
+        cell.detailTextLabel?.text = country[indexPath.row]
+        
+        let myImage = UIImage(named: "cat.png")
+        cell.imageView?.image = myImage
+        return cell
+        
+    }
+    
+    
+    
 }
 
